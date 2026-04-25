59305d5f6eed78fdbdbaa26ce58f755854e637f3
takashi-shoji66 <takashi53shoji@yahoo.co.jp>
2026-04-26 01:36:44 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// ======================================
+// Firebase設定ファイル
+// ======================================
+// Firebase Console (https://console.firebase.google.com) で
+// プロジェクトを作成後、以下の値を置き換えてください。
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBa9_F1lQ0KjyXcPjqR_WgS0cozrRfJi_o",
+  authDomain: "cam-9c80d.firebaseapp.com",
+  databaseURL: "https://cam-9c80d-default-rtdb.firebaseio.com",
+  projectId: "cam-9c80d",
+  storageBucket: "cam-9c80d.firebasestorage.app",
+  messagingSenderId: "21221548627",
+  appId: "1:21221548627:web:1b5088d0f07a2009bc1ca6"
+};
+
+// アプリ名（ページタイトルなどに使用）
+const APP_NAME = "SecureCam";
